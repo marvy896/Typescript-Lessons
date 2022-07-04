39e71d3bbbca9f4889ad706a1c6265edc4c4885f
Marvel <onyex896@gmail.com>
2022-07-04 10:35:26 +0100

    My Acad Lesson 1

---
+++ lesson1.ts
@@ -0,0 +1,41 @@
+const A1: number = 12;
+const A2 = 10;
+console.log(A1,A2);
+const A3 = A1 + A2;
+console.log(A1 + A2);
+
+
+console.log("Book");
+
+
+let B1 = 15;
+let B2 = 2;
+B1 = 4
+
+
+function F1(Name) {
+    // console.log("Hello Marvel");
+    console.log(`Hello ${Name}`)
+}
+function D2(Surname:string){
+    console.log(`your ${Surname}`);
+}
+F1("Marvel");
+
+function AddTwoNumbers(Num1:number, Num2:number) {
+    const Result = Num1 + Num2;
+    //console.log(`add ${Result}`);
+    return Result;
+}
+function MultiplyTwoNumbers(Num1:number, Num2:number){
+    const Result = Num1 * Num2;
+    return Result;
+}
+
+function F2(GetNumber:(Num1:number, Num2:number)=>number){
+    const Result = GetNumber(2,3);
+    console.log(Result);
+
+}
+F2(AddTwoNumbers)
+F2(MultiplyTwoNumbers)
